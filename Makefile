65429b9972e1ac2d44925213a3266b491744802f
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-12-02 11:34:11 +0000

    ngm space

--- Makefile
+++ Makefile
@@ -1,4 +1,14 @@
 
+# for non-make users:
+# general syntax is
+# 
+# target: dependency1 dependency2 ...
+#   commands run in the terminal
+# 
+# target & dependencies being files
+# in general, this repo follows a paradigm of listing code file as first dependency,
+# followed by inputs files to that code
+
 # reference definitions; optionally create a local.makefile to overide
 # REFDIR & FIGFMT; can also specify in invocation, e.g. `make sometarget FIGFMT=jpg`
 
@@ -17,6 +27,9 @@ COVIDM := ../covidm
 VPATH = ${INDIR}:${OUTDIR}
 
 # automates some setup tasks & provides convenience definitions
+# particular, provides definition of R such that:
+# $(call R) = Rscript [dependencies] [target]
+# $(call R,${THING}) = Rscript [dependencies] ${THING} [target]
 include support.makefile
 
 # define focal periods for estimation, by province
@@ -33,7 +46,7 @@ ${INDIR}/incidence.rds: incidence.R ${INDIR}/prov_ts_90_pub.RDS
 
 INS := $(patsubst %,${INDIR}/%.rds,timing incidence frequencies)
 
-inputs: ${INS}
+inputs: ${INS} ${INDIR}/susceptibility.rds
 
 # TBD TODO:
 # ${FIGDIR}/frequency_vis.png: something.R ${INDIR}/frequencies.rds
@@ -54,7 +67,9 @@ ${FIGDIR}/omicron_ratios.png: fig/rt_ratios.R ${OUTPUT}/omicron_ratios.rds
 # details for getting contact matrix adjustments
 include mobility.makefile
 
-${INDIR}/susceptibility.rds: susceptibility.R ${REFDIR}/escapable.rds ${REFDIR}/non_reinfectable.rds
+${INDIR}/susceptibility.rds: susceptibility.R ${DATADIR}/escapable.rds ${DATADIR}/non_reinfectable.rds
 	$(call R)
 
-${OUTDIR}/ngm_ratios.rds: ngm_ratio.R ${MOB} ${INDIR}/susceptibility.rds 
+${OUTDIR}/ngm_ratios.rds: ngm_ratio.R ${REFDIR}/contact_matrices.rds ${REFDIR}/covidm_fit_yu.qs \
+${INDIR}/susceptibility.rds ${INDIR}/timing.rds ${MOB} | ${COVIDM}
+	$(call R,${COVIDM})
---
+++ ngm_ratio.R
@@ -0,0 +1,106 @@
+suppressPackageStartupMessages({
+  require(data.table)
+  require(qs)
+})
+
+.args <- if (interactive()) c(
+  # 1-2: cm, yu data
+  file.path(
+    "refdata",
+    c("contact_matrices.rds", "covidm_fit_yu.qs")
+  ),
+  # 3-5: sus, timing, mob
+  file.path(
+    "analysis",
+    "input",
+    c("susceptibility.rds", "timing.rds", "mobility.rds")
+  ),
+  file.path("..", "SA2UK_alt", "covidm"),
+  file.path("analysis", "output", "ngm_ratio.rds")
+) else commandArgs(trailingOnly = TRUE)
+
+cms <- readRDS(.args[1])
+
+set.seed(8675309)
+yusamp <- qread(.args[2])[sample(.N, 1000, replace = FALSE)]
+
+ensemble.dt <- yusamp[, .SD, .SDcols = grep("(u|y)_", names(yusamp))][, epi_sample := 1:.N ]
+
+AR.dt <- readRDS(.args[3])
+timing <- readRDS(.args[4])
+mob.dt <- readRDS(.args[5])
+
+contact_schedule <- mob.dt[timing, on=.(region=province)][between(date,start,start+6)][, .(
+  home = 1, work = prod(work)^(1/.N),
+  other = prod(other)^(1/.N),
+  school = prod(school)^(1/.N) # school is often 0, so weight slightly differently
+), by=.(region=abbr) ]
+
+cm_path = tail(.args, 2)[1]
+cm_force_rebuild = F;
+cm_build_verbose = F;
+cm_force_shared = T
+cm_version = 2
+suppressPackageStartupMessages({
+  source(file.path(cm_path, "R", "covidm.R"))
+})
+
+calllist <- list(
+  "South Africa", "South Africa",
+  dE  = cm_delay_gamma(2.5, 2.5, t_max = 15, t_step = 0.25)$p,
+  dIp = cm_delay_gamma(1.5, 4.0, t_max = 15, t_step = 0.25)$p,
+  dIs = cm_delay_gamma(3.5, 4.0, t_max = 15, t_step = 0.25)$p,
+  dIa = cm_delay_gamma(5.0, 4.0, t_max = 15, t_step = 0.25)$p
+)
+
+calllist2 <- calllist
+calllist2$dE = cm_delay_gamma(1.25, 1.25, t_max = 15, t_step = 0.25)$p
+
+#' note: population size irrelevant to NGM calculation,
+#' and differences in provincial age distro already accounted for
+#' in contact matrices and seropos values
+params <- cm_base_parameters_SEI3R(
+  deterministic=TRUE,
+  pop=list(do.call(cm_build_pop_SEI3R, calllist))
+)
+
+params2 <- cm_base_parameters_SEI3R(
+  deterministic=TRUE,
+  pop=list(do.call(cm_build_pop_SEI3R, calllist2))
+) 
+
+us <- function(sdt.row) {
+  rep(sdt.row[, as.numeric(.SD), .SDcols = grep("^u_",names(sdt.row))], each = 2)
+}
+
+ys <- function(sdt.row) {
+  rep(sdt.row[, as.numeric(.SD), .SDcols = grep("^y_",names(sdt.row))], each = 2)
+}
+
+immune_escape <- seq(0, 1, by=0.05)
+
+assumed.AR <- AR.dt[timing, on=.(province=abbr, date=start)][,.(province, date, agegrp, escapable, non_reinfectable, inc.room = 1-non_reinfectable-escapable)]
+
+sero.scns <- rbind(
+  assumed.AR[, sero := "ref" ],
+  copy(assumed.AR)[, escapable := escapable+inc.room*0.5 ][, sero := "up" ],
+  copy(assumed.AR)[, escapable := escapable*0.5 ][, sero := "down" ]
+)
+
+ref.dt <- ensemble.dt[,{
+  u = us(.SD); y = ys(.SD)
+  sero.scns[,{
+    cs <- contact_schedule[region == province, c(home, work, other, school)]
+    testpop <- params;
+    testpop$pop[[1]]$matrices <- cms[[province]]
+    multi = sapply(immune_escape, function(imm) cm_eigen_ngm(
+      testpop,
+      u_multiplier = 1-escapable*(1-imm)-non_reinfectable,
+      contact_reductions = 1-cs,
+      uval = u,
+      yval = y)$R0)
+    .(immune_escape = immune_escape, multiplier = multi)
+  }, by=.(sero, province) ]
+}, by=epi_sample]
+
+saveRDS(ref.dt, tail(.args, 1))
