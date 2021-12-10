9951cf22710d056f3153305b709b8c77e167fd19
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-12-10 08:39:55 +0000

    updated consolidation of susceptibility inputs

--- R/susceptibility.R
+++ R/susceptibility.R
@@ -4,14 +4,17 @@ suppressPackageStartupMessages({
 })
 
 .args <- if (interactive()) c(
-    file.path("refdata", c("escapable.rds", "non_reinfectable.rds")),
+    file.path("refdata", c("Rescapable.rds", "Vescapable.rds", "non_reinfectable.rds")),
     file.path("analysis", "input", "susceptibility.rds")
 ) else commandArgs(trailingOnly = TRUE)
 
 proj.dt <- readRDS(.args[1])[
     readRDS(.args[2]),
     on = .(province, time, age),
-    .(province, date = time, age, escapable, non_reinfectable)
+    .(province, time, age, Rescapable, Vescapable)
+][readRDS(.args[3]),
+  on = .(province, time, age),
+  .(province, date = time, age, Rescapable, Vescapable, non_reinfectable)
 ]
 
 #' NGM calculation / contact matrices use 16 instead of 7 age categories,
@@ -20,7 +23,8 @@ AR.dt <- proj.dt[,
     .(
         date = rep(date[1], 16),
         agegrp = 1:16,
-        escapable = escapable[c(rep(1, 3), rep(2, 4), rep(3, 5), 4:7)],
+        Rescapable = Rescapable[c(rep(1, 3), rep(2, 4), rep(3, 5), 4:7)],
+        Vescapable = Vescapable[c(rep(1, 3), rep(2, 4), rep(3, 5), 4:7)],
         non_reinfectable = non_reinfectable[
             c(rep(1, 3), rep(2, 4), rep(3, 5), 4:7)
         ]
