8c0381ea9630505dc1df87c910c7841415ef1034
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-12-18 22:29:04 +0000

    incorporate end date retargetting

--- R/fig_frequency_fit.R
+++ R/fig_frequency_fit.R
@@ -6,12 +6,12 @@ suppressPackageStartupMessages({
     require(bbmle)
 })
 
-.debug <- c("2021-12-06", "2021-11-27")[1]
+.debug <- c("2021-12-06", "2021-11-27")[2]
 .args <- if (interactive()) c(
     file.path("analysis", "input", "sgtf.rds"),
     file.path("analysis", "output", "sgtf", .debug, "mergedfit.rds"),
     file.path("analysis", "output", "sgtf", .debug, "sims.rds"),
-    file.path("analysis", "output", "fig", "frequencies.png")
+    file.path("analysis", "output", "fig", .debug, "frequencies.png")
 ) else commandArgs(trailingOnly = TRUE)
 
 regionkey = c(
@@ -27,7 +27,9 @@ regionkey = c(
     ALL="ALL"
 )
 
-sgtf.dt <- readRDS(.args[1])[date <= "2021-12-06"]
+enddate <- as.Date(basename(dirname(tail(.args, 1))))
+
+sgtf.dt <- readRDS(.args[1])[date <= enddate]
 min.date <- sgtf.dt[, min(date)]
 
 fit.ref <- sgtf.dt[,.(
@@ -79,9 +81,9 @@ plotter <- function(dt, fit.dt, ens.dt, start.date = "2021-10-15") ggplot(dt[dat
     scale_y_continuous("Proportion (logit scale)", trans="logit", breaks = c(0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99)) +
     scale_size_area(
         "Samples", breaks = sqrt(1/c(1,10,100,1000)), max_size = 10, labels = function(b) 1/b^2,
-        guide = guide_legend(override.aes = list(alpha = c(0.1,0.3,0.7,1)))
+        guide = guide_legend(override.aes = list(alpha = c(0.1,0.3,0.7)))
     ) +
-    scale_x_date(NULL, date_breaks = "months", date_minor_breaks = "weeks", date_labels = "%b") +
+    scale_x_date("Specimen receipt date", date_breaks = "months", date_minor_breaks = "weeks", date_labels = "%b") +
     scale_color_manual(NULL, values = c(observation="black", ensemble = "firebrick", `est. SGTF`="dodgerblue", `est. BA.1`="red")) +
     scale_alpha_continuous(NULL, range = c(0.1, 1), guide = "none")
 
