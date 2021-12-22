ca5f213d5ae00b4df57586a8bee8d7970b9d3380
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-12-22 18:03:39 +0000

    integrate plot reference

--- R/fig/fig_freq_fit_compare.R
+++ R/fig/fig_freq_fit_compare.R
@@ -9,25 +9,11 @@ suppressPackageStartupMessages({
 .args <- if (interactive()) c(
     file.path("analysis", "input", "sgtf.rds"),
     file.path("analysis", "output", "sgtf"),
-    file.path("analysis", "output", "fig", "cross_frequencies.png")
+    file.path("analysis", "input", "plotref.rda"),
+    file.path("analysis", "output", "fig", "stgf_model_fit_comparison.png")
 ) else commandArgs(trailingOnly = TRUE)
 
-regionkey = c(
-    EC="EASTERN CAPE",
-    FS="FREE STATE",
-    GP="GAUTENG",
-    KZN="KWAZULU-NATAL",
-    LP="LIMPOPO",
-    MP="MPUMALANGA",
-    NC="NORTHERN CAPE",
-    NW="NORTH WEST",
-    WC="WESTERN CAPE",
-    ALL="ALL"
-)
-
-enddate <- as.Date(basename(dirname(tail(.args, 1))))
-
-sgtf.dt <- readRDS(.args[1])[date <= enddate]
+sgtf.dt <- readRDS(.args[1])
 min.date <- sgtf.dt[, min(date)]
 
 fit.ref <- sgtf.dt[,.(
@@ -70,21 +56,21 @@ plot.dt[,
     )
 ]
 
+#' load conserved plotting elements
+load(tail(.args, 2)[1])
+
 p <- ggplot(plot.dt) + aes(date, linetype=model) +
     facet_grid(end.date ~ prov) +
     geom_line(aes(y=SGTF_est, color = "SGTF est")) +
     geom_line(aes(y=BA.1_est, color = "BA.1 est")) +
-    geom_point(aes(y=SGTF/total, size = sqrt(1/total), alpha = sqrt(total), color = "observation"), data = function(dt) dt[date <= end.date]) +
+    geom_point(aes(y=SGTF/total, size = total, alpha = total, color = "observation"), data = function(dt) dt[date <= end.date]) +
     theme_minimal(base_size = 14) +
     coord_cartesian(ylim=c(0.01, 0.99)) +
-    scale_y_continuous("Proportion (logit scale)", trans="logit", breaks = c(0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99)) +
-    scale_size_area(
-        "Samples", breaks = sqrt(1/c(1,10,100,1000)), max_size = 10, labels = function(b) 1/b^2,
-        guide = guide_legend(override.aes = list(alpha = c(0.1,0.3,0.7,1)))
-    ) +
-    scale_x_date("Sample receipt date", date_breaks = "months", date_minor_breaks = "weeks", date_labels = "%b") +
-    scale_alpha_continuous(NULL, range = c(0.1, 1), guide = "none") +
-    scale_linetype("Fit Model") +
+    scale_y_logitprop() +
+    scale_size_samples() +
+    scale_alpha_samples() +
+    scale_x_recieptdate() +
+    scale_linetype("Fit Model", guide = "none") +
     scale_color_discrete(NULL) +
     theme(legend.position = "bottom")
 
