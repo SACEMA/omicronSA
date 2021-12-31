724bdcf8cc66d836535f9327f4500c76f1931472
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-12-31 08:28:54 +0000

    update to use new fitting pipeline outputs; still need to in-source fitting steps

--- R/ensemble.R
+++ R/ensemble.R
@@ -1,6 +1,7 @@
 
 suppressPackageStartupMessages({
     require(data.table)
+    require(emdbook)
 })
 
 .debug <- c("2021-11-27", "2021-12-06")[2]
@@ -10,7 +11,7 @@ suppressPackageStartupMessages({
             "analysis", "input", "incidence.rds"
         ),
         file.path(
-            "analysis", "output", "sgtf", .debug, "sims.rds"
+            "analysis", "output", "sgtf", .debug, "fits.rds"
         ),
         file.path(
             "analysis", "output", .debug, "incidence_ensemble.rds"
@@ -19,6 +20,8 @@ suppressPackageStartupMessages({
 } else {
     commandArgs(trailingOnly = TRUE)
 }
+
+day0 <- as.Date("2021-09-24")
 
 regionkey = c(
     EC="EASTERN CAPE",
@@ -34,24 +37,32 @@ regionkey = c(
 )
 
 end.date <- as.Date(basename(dirname(tail(.args, 1))))
-sims <- readRDS(.args[2])[date <= end.date]
+fits <- as.data.table(readRDS(.args[2]))
+fits[, province := regionkey[as.character(prov)] ]
 
-sims[, province := regionkey[as.character(prov)] ]
-
-#' TODO note need to exclude sample 64, 80 for negative slopes
-#' fine with the current 50 sample cut off
-sims[,any(diff(prop)<0),by=.(prov, sample)][V1==TRUE]
-
-#' TODO: naming => preference order
-#' something like pref_XX_MODELNAME
+baselogis <- function(
+    tvec, # observation times
+    loc, delta_r, # fit parameters
+    lodrop = -Inf, logain = -Inf
+){
+    drop <- plogis(lodrop)
+    gain <- plogis(logain)
+    ptrue <- plogis((tvec-loc)*delta_r)
+    
+    return(ptrue*(1-gain) + (1-ptrue)*drop)
+}
 
 set.seed(8675309)
 inc.dt <- readRDS(.args[1])[
-    sims, on = .(date, province), allow.cartesian = TRUE, nomatch = 0
-][,
-    var := rbinom(.N, tot, prop)
-][, # TODO: consider looking only at primary infections
-    inf1var := rbinom(.N, inf1, prop)
+    fits, on = .(province), allow.cartesian = TRUE, nomatch = 0
+][between(date, day0, end.date)][, time := as.numeric(date - day0) ][,
+    var := {
+        #' TODO split with split estimates
+        propreinf <- propprimary <- baselogis(time, loc, deltar)
+        dp <- rbetabinom(.N, propprimary, inf1, beta_shape)
+        dr <- rbetabinom(.N, propreinf, tot-inf1, beta_shape)
+        dp + dr
+    }
 ]
 
-saveRDS(inc.dt, tail(.args, 1))
+saveRDS(inc.dt[, .(sample = sample_no, prov, date, ref = tot - var, var)], tail(.args, 1))
