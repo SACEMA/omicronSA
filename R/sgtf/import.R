0f42b331a6bb5e2c825ddde4ca792916675f93ce
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2022-02-18 16:24:52 +0000

    more WIP re negative tests

--- R/sgtf/import.R
+++ R/sgtf/import.R
@@ -1,7 +1,6 @@
 
-suppressPackageStartupMessages({
-    stopifnot(all(sapply(c("data.table", "haven"), require, character.only = TRUE)))
-})
+.pkgs <- c("data.table", "haven")
+stopifnot(all(sapply(.pkgs, require, character.only = TRUE, quietly = TRUE)))
 
 .args <- if (interactive()) file.path("refdata", c(
     "sgtf_list_anon_20220131.dta",
@@ -13,41 +12,49 @@ suppressPackageStartupMessages({
 #' make rules should redirect this to appropriate log
 warn <- function(ws, to = stderr()) invisible(sapply(ws, function(w) write(w, file = to)))
 
-#' get negative tests to potentially split multi test individuals
-neg.ref <- readRDS(.args[2])[, .(
-	caseid_hash, province, publicprivateflag = sector,
-	ct30 = FALSE, sgtf = -1,
-	specreceiveddate = as.Date(specreceiveddate),
-	specreportdate = as.Date(specreportdate),
-	speccollectiondate = as.Date(speccollectiondate)
-)]
-
 #' n.b. factor provinces here are not the reference factor levels:
 #' just used for later cleaning steps at this stage in code
+#' the `as...` conversions cleanup the SASS conversion to standard
+#' data types
 sgtf.raw <- as.data.table(read_dta(.args[1]))[,.(
-    caseid_hash,
+    caseid_hash = as.character(caseid_hash),
     province = factor(
         as.character(province),
         levels = as.character(unique(province))
     ),
-    publicprivateflag = as.factor(publicprivateflag),
-    ct30 = as.logical(as.integer(ct30)),
+    # publicprivateflag = as.factor(publicprivateflag),
+    # ct30 = as.logical(as.integer(ct30)),
     #' note: only ct30 == TRUE results in inputs
+    #' and publicprivateflag not used in decisions
     sgtf = as.integer(sgtf),
     specreceiveddate = as.Date(specreceiveddate),
     specreportdate = as.Date(specreportdate),
     speccollectiondate = as.Date(speccollectiondate)
 )]
 
+unloadNamespace("haven")
+
+#' get negative tests to potentially split multi test individuals
+#' TODO? read in more complete test- data, filter to relevant tests
+neg.ref <- readRDS(.args[2])[, .(
+	caseid_hash, province,
+	sgtf = -1L,
+	specreceiveddate = as.Date(specreceiveddate),
+	specreportdate = as.Date(specreportdate),
+	speccollectiondate = as.Date(speccollectiondate)
+)]
+
+#' remove records with `is.na(sgtf)` results - these won't be usable
+#' in analysis
 sgtf.clean <- setkey(sgtf.raw[!is.na(sgtf)], province, specreceiveddate)
 
+#' TODO: more detailed notes?
 if (sgtf.raw[,.N] != sgtf.clean[,.N]) {
-    #' TODO: more detailed warning?
     #' currently, the sgtf == NA records are the only entries for these
-    #' caseids, but if these individuals did have conclusive results for other
-    #' tests, then would be fine
+    #' caseid_hash, but if these individuals did have conclusive results for other
+    #' tests, might want to note?
     warn(c(
-        "WARN: Removing records with NA sgtf results.",
+        "WARN: Removing records with `is.na(sgtf) == TRUE` results.",
         sprintf(
             "%i caseid_hashes out of %i (%f%%):",
             sgtf.raw[is.na(sgtf), length(unique(caseid_hash))],
@@ -67,29 +74,67 @@ if (sgtf.raw[,.N] != sgtf.clean[,.N]) {
 #' n.b. assumes there are no receipt dates or report dates that are NA
 correctingview <- expression(
 	(specreceiveddate == specreportdate) &
-		(as.numeric(specreceiveddate - speccollectiondate) > 7)
+	(as.numeric(specreceiveddate - speccollectiondate) > 7)
 )
 if (sgtf.clean[eval(correctingview), .N]) {
     cview <- sgtf.clean[eval(correctingview)]
     warn(c(
         sprintf(
-            "WARN: %i records have specimen receipt dates > specimen collection date + 7.",
+            "WARN: %i test+, sgtf records have specimen receipt dates > specimen collection date + 7.",
             cview[,.N]
         ),
         "These caseid_hash will use collection date vice receipt date:",
         cview[, paste(caseid_hash, " : ",  speccollectiondate, " vice ", specreceiveddate, sep = "", collapse ="\n")]
+    ))
+    ncview <- neg.ref[eval(correctingview)]
+    warn(c(
+    	sprintf(
+    		"WARN: %i test- records have specimen receipt dates > specimen collection date + 7.",
+    		ncview[,.N]
+    	),
+    	"These caseid_hash will use collection date vice receipt date:",
+    	ncview[, paste(caseid_hash, " : ",  speccollectiondate, " vice ", specreceiveddate, sep = "", collapse ="\n")]
     ))
 }
 
 sgtf.clean[,
     date := fifelse(eval(correctingview), speccollectiondate, specreceiveddate)
 ]
-#' also apply shift to test- data
 neg.ref[,
 	date := fifelse(eval(correctingview), speccollectiondate, specreceiveddate)
 ]
 
-sgtf.clean[order(date), test := 1:.N, by=caseid_hash]
+sgtf.clean[order(date),
+	c("test", "delay") := .(
+		1:.N,
+		c(0, as.integer(diff(date)))
+	),
+	by=caseid_hash
+]
+
+short.threshold <- 15
+
+splitting.records <- rbind(
+	sgtf.clean, neg.ref[, c("test", "delay") := .(-1, -1), by=caseid_hash ]
+)[
+	order(date),
+	#' primarily, we want to order by date
+	#' however, resolving ties by date depends on test- vs test+
+	#' in a complex way. basically, want to minimize the number of
+	#' - vs + runs in the data, which means sometimes + comes before -
+	#' and vice versa
+	{
+		
+	},
+	delay := nafill(delay, "nocb"),
+	by=caseid_hash
+][
+	((test < 0) & (delay > short.threshold)) | ((test < -1)),
+	.SD[.N], #' taking the last entry, as that gives the number of sequential negative tests
+	by=caseid_hash
+]
+
+
 
 #' all multi-test episodes have test == 2
 multi.episode <- sgtf.clean[test == 2, caseid_hash]
@@ -100,13 +145,13 @@ sgtf.singular <- sgtf.clean[
 	.(caseid_hash, province, sgtf, date, episode = 1)
 ]
 
+?nafill
+
 #' now deal with multi-test episodes, by setting a short threshold
 #' for simple consolidation vs potentially splitting test outcomes
 #' into multiple episodes
 
-short.threshold <- 28
-
-#' define short, multi-test episodes as all tests short.threshold days
+#' identify all the multi-test episodes spans
 all.spans <- sgtf.clean[
     caseid_hash %in% multi.episode,
     {
@@ -124,16 +169,28 @@ all.spans <- sgtf.clean[
     by=caseid_hash
 ]
 
+#' over a short period of time, an observed SG* status change could be due
+#' to e.g. random probe failure when near the detection threshold early or
+#' late in the infection course. The same logic applies to intermediate
+#' test- results.
+#' 
+#' for short periods only:
+#' if we observe >1 intermediate test- AND a change in SG* status
+#' then split into multiple SG* episodes
+#' 
+#' for total spans
+
+
+#' based on Hay et al: https://dash.harvard.edu/handle/1/37370587
+#' by inspection, all Ct trajectories fall w/in the 15 day window
+#' Anything longer this window should be considered with more
+#' nuance, but assume anything w/in is the same event
+
 short.spans <- all.spans[span < short.threshold]
 
-#' if we observe a negative intermediate test AND a change in SG* status
 short.splits <- neg.ref[
 	short.spans, on=.(caseid_hash), nomatch=0
 ][(changes == TRUE) & between(date, start, end, incbounds = FALSE)]
-#' TODO: in data as-is, no short splits
-#' however, need to write code to incorporate if there were any
-#' basic gist would be to split on the negative tests, then coalesce as below
-
 short.episodes <- short.spans[, caseid_hash]
 
 #' coalesce as:
