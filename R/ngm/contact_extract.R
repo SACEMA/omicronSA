36cd459259fd72f4ccf84e347cb78b8e9b6c6fce
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2022-06-23 16:05:12 +0100

    notes on contact extraction

---
+++ R/ngm/contact_extract.R
@@ -0,0 +1,25 @@
+90 young, 10 old
+
+50 total contacts (1 per person per day)
+
+1 person generates 0.5 contact per day
+
+describing effective contacts, say:
+1 young person generates 0.03 contacts with old person
+1 old person generates 0.3 contacts with young person
+
+check:
+100 young people = 3 contacts with old
+10 old = 3 contacts with young
+
+if 33 infectious young people = ~1 infectious exposure to old person per time
+
+I2dot = ... + 0.03 * 33 * 10/10 = ~1 per time
+
+then if old people are 1/2 previously exposed (S_old/N_old = .5), then 50% chance that infectious exposure works
+
+I2dot = ... + 0.03 * 33 * 5/10 = ~0.5 per time
+
+if there are 3 infectious old people, then
+
+I1dot = ... + 0.3 * 3 * 67/100 = 2/3
